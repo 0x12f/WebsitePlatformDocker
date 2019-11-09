5a8835db503dfaca11794f746c13750a7344bc39
Aleksey Ilyin <alksily@icloud.com>
2019-11-09 17:21:36 +0200

    add update.sh add settings.php

--- install.sh
+++ install.sh
@@ -33,6 +33,6 @@ docker-compose run php vendor/bin/doctrine dbal:run-sql "INSERT INTO user_sessio
 docker-compose run php vendor/bin/doctrine dbal:run-sql "INSERT INTO user (uuid, username, email, password, status, level) VALUES ('00000000-0000-0000-0000-000000000000', 'admin', 'admin@example.com', '4b60602435c81eca6516601b68219c37f93de49c1192660aaa16066070e23b352fb0578b30cb588bb416b5138f03511a809f8b6610d20d90bf72d2a4d9e9548e06cd3eec8ed6', 'work', 'admin');"
 
 # Save current engine version into file
-cd engine && git log -n1 --pretty='%h' > ../var/version.txt
+cd engine && git log -n1 --pretty='%h' > ../var/version_hash.txt && git describe --exact-match --tags $(git log -n1 --pretty='%h') > ../var/version_tag.txt
 
 echo "done"
